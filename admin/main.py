249871cae887a368dbe40b759df513c78ee375f6
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-10-14 16:16:11 +0300

    lorem

--- admin/main.py
+++ admin/main.py
@@ -7,7 +7,7 @@ import uvicorn
 if __name__ == '__main__':
     uvicorn.run('app:app',
                 host='0.0.0.0',
-                port=80,
-                ssl_keyfile='',
-                ssl_certfile='',
+                port=8080,
+                ssl_keyfile='mykey.key',
+                ssl_certfile='mycert.crt',
                 reload=True)
